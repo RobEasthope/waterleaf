a80806e5eb558c5e8d696e75ad1f80fbf67a0332
Rob Easthope <hello@robeasthope.com>
2024-03-25 14:30:31 +0000

    Deconstruct props

--- app/sanity/components/Duration/Duration.tsx
+++ app/sanity/components/Duration/Duration.tsx
@@ -1,16 +1,13 @@
 import { Box, Code, Flex } from '@sanity/ui';
-import React from 'react';
 import type { NumberInputProps } from 'sanity';
 
 import { secondsToMinutes } from '~/utils/secondsToMinutes';
 
-export default function Duration(props: NumberInputProps) {
+export default function Duration({ renderDefault, value }: NumberInputProps) {
   return (
     <Flex gap={3} align="center">
-      <Box flex={1}>{props.renderDefault(props)}</Box>
-      {props.value ? (
-        <Code size={4}>{secondsToMinutes(props.value)}</Code>
-      ) : null}
+      <Box flex={1}>{renderDefault(renderDefault, value)}</Box>
+      {value ? <Code size={4}>{secondsToMinutes(value)}</Code> : null}
     </Flex>
   );
 }
