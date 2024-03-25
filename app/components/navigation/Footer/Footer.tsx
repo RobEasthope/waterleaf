427d3794ebce775c175f40e7a82dff446f6e5440
Rob Easthope <hello@robeasthope.com>
2024-03-25 14:52:48 +0000

    Remove props

--- app/components/navigation/Footer/Footer.tsx
+++ app/components/navigation/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-export function Footer({ home }: LogoProps) {
+export function Footer() {
   return (
     <header className="border-t border-gray-100 transition-colors duration-1000 ease-in-out dark:border-gray-900">
       <div className="container mx-auto flex items-center justify-between p-4 lg:px-12">
