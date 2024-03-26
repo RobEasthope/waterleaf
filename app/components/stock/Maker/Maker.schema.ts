f8488773279ac16236092cbe9f6b7b89b0b6625e
Rob Easthope <hello@robeasthope.com>
2024-03-26 09:24:42 +0000

    Create Maker.schema.ts

---
+++ app/components/stock/Maker/Maker.schema.ts
@@ -0,0 +1,22 @@
+import { RxAvatar } from 'react-icons/rx';
+import { defineField, defineType } from 'sanity';
+
+export const Maker = defineType({
+  name: 'Maker',
+  title: 'Naker',
+  type: 'document',
+  icon: RxAvatar,
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+    }),
+  ],
+});
