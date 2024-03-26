7d2ff3b5addd601055c5f206b6d1ce59e7e2928c
Rob Easthope <hello@robeasthope.com>
2024-03-26 12:09:46 +0000

    Add AppSettings to schema index

--- app/sanity/schema-index.ts
+++ app/sanity/schema-index.ts
@@ -1,5 +1,6 @@
+import { AppSettings } from '~/components/settings/AppSettings/AppSettings.schema';
 import { Collection } from '~/components/stock/Collection/Collection.schemas';
 import { Maker } from '~/components/stock/Maker/Maker.schema';
 import { Stock } from '~/components/stock/Stock/Stock.schema';
 
-export default [Stock, Maker, Collection];
+export default [Stock, Maker, Collection, AppSettings];
