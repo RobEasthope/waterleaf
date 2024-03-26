a37362e08d410204ba5b8469b41350a9ce5758bf
Rob Easthope <hello@robeasthope.com>
2024-03-26 11:58:41 +0000

    Add maker and collection to index

--- app/sanity/schema-index.ts
+++ app/sanity/schema-index.ts
@@ -1,3 +1,5 @@
+import { Collection } from '~/components/stock/Collection/Collection.schemas';
+import { Maker } from '~/components/stock/Maker/Maker.schema';
 import { Stock } from '~/components/stock/Stock/Stock.schema';
 
-export default [Stock];
+export default [Stock, Maker, Collection];
