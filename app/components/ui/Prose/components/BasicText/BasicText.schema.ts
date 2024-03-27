16cf9a87b10db0f5d6c1fc5447f47f05ed93c17d
Rob Easthope <hello@robeasthope.com>
2024-03-27 10:03:36 +0000

    Copy in Prose component

---
+++ app/components/ui/Prose/components/BasicText/BasicText.schema.ts
@@ -0,0 +1,37 @@
+import { defineArrayMember, defineType } from "@sanity-typed/types";
+import { createBlockValidator } from 'sanity-pills';
+
+
+const basicTextValidator = createBlockValidator({
+  required: false,
+  noEmptyBlocks: true,
+  validateLinks: true,
+  styleRequired: true,
+  noStackedMarks: true,
+  noNewlines: true,
+  noTerminatingWhitespace: true,
+});
+
+export default defineType({
+  name: 'BasicText',
+  title: 'Basic text',
+  type: 'array',
+  of: [
+    defineArrayMember({
+      type: 'block',
+      styles: [],
+      marks: {
+        decorators: [
+          { title: 'Strong', value: 'strong' },
+          { title: 'Emphasis', value: 'em' },
+        ],
+        annotations: [
+          { type: 'InternalLink' },
+          { type: 'ExternalLink' },
+          { type: 'EmailLink' },
+        ],
+      },
+    }),
+  ],
+  validation: basicTextValidator,
+});
