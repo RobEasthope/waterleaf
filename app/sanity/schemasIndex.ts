7947388c4ff668c0e48e813bd89de13351c3c179
Rob Easthope <hello@robeasthope.com>
2024-03-27 12:59:20 +0000

    Add links to schema index

--- app/sanity/schemasIndex.ts
+++ app/sanity/schemasIndex.ts
@@ -1,6 +1,24 @@
+import EmailLinkSchema from '~/components/navigation/EmailLink/EmailLink.schema';
+import EmailLinkWithTitleSchema from '~/components/navigation/EmailLink/EmailLinkWithTitle.schema';
+import ExternalLinkSchema from '~/components/navigation/ExternalLink/ExternalLink.schema';
+import ExternalLinkWithTitleSchema from '~/components/navigation/ExternalLink/ExternalLinkWithTitle.schema';
+import InternalLinkSchema from '~/components/navigation/InternalLink/InternalLink.schema';
+import InternalLinkWithTitleSchema from '~/components/navigation/InternalLink/InternalLinkWithTitle.schema';
 import { AppSettings } from '~/components/settings/AppSettings/AppSettings.schema';
 import { Collection } from '~/components/stock/Collection/Collection.schemas';
 import { Maker } from '~/components/stock/Maker/Maker.schema';
 import { Stock } from '~/components/stock/Stock/Stock.schema';
 
-export const schemasIndex = [Stock, Maker, Collection, AppSettings];
+export const schemasIndex = [
+  // Links
+  EmailLinkSchema,
+  EmailLinkWithTitleSchema,
+  ExternalLinkSchema,
+  ExternalLinkWithTitleSchema,
+  InternalLinkSchema,
+  InternalLinkWithTitleSchema,
+  Stock,
+  Maker,
+  Collection,
+  AppSettings,
+];
