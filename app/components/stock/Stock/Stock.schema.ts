dabdb38cb24e6857e4a6fa87384f37f01fae45bb
Rob Easthope <hello@robeasthope.com>
2024-04-08 10:47:17 +0100

    Draft out stock field groups

--- app/components/stock/Stock/Stock.schema.ts
+++ app/components/stock/Stock/Stock.schema.ts
@@ -8,6 +8,20 @@ export default defineType({
   title: 'Stock',
   type: 'document',
   icon: BsBrush,
+  groups: [
+    {
+      name: 'description',
+      title: 'Description',
+    },
+    {
+      name: 'imagery',
+      title: 'Imagery',
+    },
+    {
+      name: 'config',
+      title: 'Commerce',
+    },
+  ],
   fieldsets: [
     { name: 'dimensions', title: 'Dimensions', options: { columns: 2 } },
   ],
@@ -16,18 +30,21 @@ export default defineType({
       name: 'title',
       title: 'Title',
       type: 'string',
+      group: 'description',
       validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'shortDescription',
       title: 'Short description',
       type: 'string',
+      group: 'description',
       validation: (Rule) => Rule.required().error(),
     }),
     defineField({
       name: 'price',
       title: 'Price',
       type: 'number',
+      group: 'description',
       validation: (Rule) => Rule.required(),
     }),
     defineField({
@@ -38,6 +55,7 @@ export default defineType({
         list: [{ title: 'Map', value: 'map' }],
       },
       initialValue: 'map',
+      group: 'config',
       validation: (Rule) => Rule.required(),
     }),
     defineField({
@@ -45,12 +63,14 @@ export default defineType({
       title: 'Single item',
       type: 'boolean',
       initialValue: true,
+      group: 'config',
       validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'quantity',
       title: 'Quantity',
       type: 'number',
+      group: 'config',
       hidden: ({ parent, value }) => !value && parent?.singleItem,
     }),
     defineField({
@@ -58,6 +78,7 @@ export default defineType({
       title: 'Active',
       type: 'boolean',
       initialValue: false,
+      group: 'config',
       validation: (Rule) => Rule.required(),
     }),
     defineField({
@@ -72,6 +93,7 @@ export default defineType({
       title: 'Cartographer/Author',
       type: 'reference',
       to: [{ type: 'Maker' }],
+      group: 'description',
       validation: (Rule) => Rule.required().error(),
     }),
     defineField({
@@ -96,6 +118,7 @@ export default defineType({
       name: 'description',
       title: 'Full stock description',
       type: 'StockProse',
+      group: 'description',
       validation: (Rule) => Rule.required().error(),
     }),
     defineField({
@@ -103,18 +126,21 @@ export default defineType({
       title: 'Images',
       type: 'array',
       of: [defineArrayMember({ type: 'StockImage' })],
+      group: 'imagery',
       validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'width',
       title: 'Width',
       type: 'number',
+      group: 'description',
       fieldset: 'dimensions',
     }),
     defineField({
       name: 'height',
       title: 'Height',
       type: 'number',
+      group: 'description',
       fieldset: 'dimensions',
     }),
     defineField({
@@ -124,12 +150,14 @@ export default defineType({
       options: {
         source: 'title',
       },
+      group: 'config',
       validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'stockId',
       title: 'Stock ID',
       type: 'string',
+      group: 'config',
       validation: (Rule) => Rule.required().error(),
     }),
   ],
