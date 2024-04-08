0c6afa346ab0a23698d4dea841d2878ec6ac81a0
Rob Easthope <hello@robeasthope.com>
2024-04-08 11:08:34 +0100

    Fix collection slug generation

--- app/components/stock/Collection/Collection.schemas.ts
+++ app/components/stock/Collection/Collection.schemas.ts
@@ -22,7 +22,7 @@ export default defineType({
       name: 'slug',
       type: 'slug',
       options: {
-        source: 'title',
+        source: 'name',
       },
       validation: (Rule) => Rule.required(),
     }),
