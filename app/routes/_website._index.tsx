fbde9dadecbc0eb99bc0e1ef61fe641b30409a6c
Rob Easthope <hello@robeasthope.com>
2024-04-09 09:57:10 +0100

    Strip back markup

--- app/routes/_website._index.tsx
+++ app/routes/_website._index.tsx
@@ -1,36 +1,36 @@
-import type {LoaderFunctionArgs, MetaFunction} from '@remix-run/node'
-import {json} from '@remix-run/node'
-import {useLoaderData} from '@remix-run/react'
-import {useQuery} from '@sanity/react-loader'
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { useQuery } from '@sanity/react-loader';
 
-import {Loading} from '~/components/Loading'
-import {Records} from '~/components/Records'
-import type {loader as layoutLoader} from '~/routes/_website'
-import {loadQuery} from '~/sanity/loader.server'
-import {loadQueryOptions} from '~/sanity/loadQueryOptions.server'
-import {RECORDS_QUERY} from '~/sanity/queries'
-import type {RecordStub} from '~/types/record'
-import {recordStubsZ} from '~/types/record'
+import { Loading } from '~/components/Loading';
+import { Records } from '~/components/Records';
+import type { loader as layoutLoader } from '~/routes/_website';
+import { loadQuery } from '~/sanity/loader.server';
+import { loadQueryOptions } from '~/sanity/loadQueryOptions.server';
+import { RECORDS_QUERY } from '~/sanity/queries';
+import type { RecordStub } from '~/types/record';
+import { recordStubsZ } from '~/types/record';
 
 export const meta: MetaFunction<
   typeof loader,
   {
-    'routes/_website': typeof layoutLoader
+    'routes/_website': typeof layoutLoader;
   }
-> = ({matches}) => {
+> = ({ matches }) => {
   const layoutData = matches.find(
     (match) => match.id === `routes/_website`,
-  )?.data
-  const home = layoutData ? layoutData.initial.data : null
-  const title = [home?.title, home?.siteTitle].filter(Boolean).join(' | ')
+  )?.data;
+  const home = layoutData ? layoutData.initial.data : null;
+  const title = [home?.title, home?.siteTitle].filter(Boolean).join(' | ');
 
-  return [{title}]
-}
+  return [{ title }];
+};
 
-export const loader = async ({request}: LoaderFunctionArgs) => {
-  const {options} = await loadQueryOptions(request.headers)
-  const query = RECORDS_QUERY
-  const queryParams = {}
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { options } = await loadQueryOptions(request.headers);
+  const query = RECORDS_QUERY;
+  const queryParams = {};
   const initial = await loadQuery<RecordStub[]>(
     query,
     queryParams,
@@ -38,22 +38,22 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
   ).then((res) => ({
     ...res,
     data: res.data ? recordStubsZ.parse(res.data) : null,
-  }))
+  }));
 
   if (!initial.data) {
-    throw new Response('Not found', {status: 404})
+    throw new Response('Not found', { status: 404 });
   }
 
   return json({
     initial,
     query,
     params: queryParams,
-  })
-}
+  });
+};
 
 export default function Index() {
-  const {initial, query, params} = useLoaderData<typeof loader>()
-  const {data, loading, encodeDataAttribute} = useQuery<typeof initial.data>(
+  const { initial, query, params } = useLoaderData<typeof loader>();
+  const { data, loading, encodeDataAttribute } = useQuery<typeof initial.data>(
     query,
     params,
     {
@@ -61,18 +61,14 @@ export default function Index() {
       // @ts-expect-error
       initial,
     },
-  )
+  );
 
   if (loading && !data) {
-    return <Loading />
-  } else if (!data || !initial.data) {
-    return <div>Not found</div>
+    return <Loading />;
+  }
+  if (!data || !initial.data) {
+    return <div>Not found</div>;
   }
 
-  return (
-    <Records
-      records={data || initial.data}
-      encodeDataAttribute={encodeDataAttribute}
-    />
-  )
+  return <div>Home</div>;
 }
