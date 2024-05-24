91894d80e89d1b79f8fa5e8f78c132b18601a7fb
Rob Easthope <hello@robeasthope.com>
2024-05-24 08:34:53 +0100

    Refactor test

--- app/components/ui/Box/Box.test.tsx
+++ app/components/ui/Box/Box.test.tsx
@@ -1,11 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { Box } from './Box';
-import { describe, expect, it } from 'vitest';
+import { expect, test } from 'vitest';
 
-describe('App', () => {
-  it('renders children', () => {
-    render(<Box as="div">Lorem ipsum</Box>);
+test('renders children', () => {
+  render(<Box as="div">Lorem ipsum</Box>);
 
-    expect(screen.queryAllByText(/Lorem ipsum/i)).toBeDefined();
-  });
+  expect(screen.queryAllByText(/Lorem ipsum/i)).toBeDefined();
 });
