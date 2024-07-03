69dedc80908c69989b7007a791e4b78a0796f0ac
Rob Easthope <hello@robeasthope.com>
2024-07-03 18:34:04 +0100

    Copy in Text block

---
+++ app/components/generic/Text/Text.schema.ts
@@ -0,0 +1,24 @@
+import { BiText } from 'react-icons/bi';
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  type: 'object',
+  name: 'Text',
+  title: 'Text',
+  icon: BiText,
+  fields: [
+    defineField({
+      name: 'text',
+      title: 'Text',
+      type: 'FullProse',
+      validation: (Rule) => Rule.required().warning('Text: Text is missing'),
+    }),
+  ],
+  preview: {
+    prepare() {
+      return {
+        title: 'Text',
+      };
+    },
+  },
+});
