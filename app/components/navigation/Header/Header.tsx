1d6e16d328658886c0274c6281b79593186edf42
Rob Easthope <hello@robeasthope.com>
2024-07-03 18:24:56 +0100

    Restore header and footer code

--- app/components/navigation/Header/Header.tsx
+++ app/components/navigation/Header/Header.tsx
@@ -1,14 +1,7 @@
-import { Logo } from "~/components/_unsorted/Logo/Logo";
-import { ThemeToggle } from "~/components/_unsorted/ThemeToggle/ThemeToggle";
-import type { LayoutProps } from "~/components/Layout";
-
-export function Header(props: LayoutProps) {
+export function Header() {
   return (
-    <header className="border-b border-gray-100 transition-colors duration-1000 ease-in-out dark:border-gray-900">
-      <div className="container mx-auto flex items-center justify-between p-4 lg:px-12">
-        <Logo home={props.home} />
-        <ThemeToggle theme={props.theme} />
-      </div>
+    <header>
+      <div>Header</div>
     </header>
   );
 }
