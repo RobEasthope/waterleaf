e9ad070a51242a20f1ad51d5193cbb385cc993e9
Rob Easthope <hello@robeasthope.com>
2024-07-12 15:00:16 +0100

    Remove import

--- app/routes/_app._index.tsx
+++ app/routes/_app._index.tsx
@@ -2,7 +2,6 @@ import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { useQuery } from "@sanity/react-loader";
 
-import { Records } from "~/components/_unsorted/Records/Records";
 import { loadQuery } from "~/components/sanity/loader.server";
 import { loadQueryOptions } from "~/components/sanity/loadQueryOptions.server";
 import { RECORDS_QUERY } from "~/components/sanity/queries";
