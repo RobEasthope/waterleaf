a0cdb3e9fd8f0b12fafc8d360b4ae95506c7b7ce
Rob Easthope <hello@robeasthope.com>
2024-07-12 15:11:49 +0100

    Ignore TS issues

--- app/utils/useRootLoaderData.ts
+++ app/utils/useRootLoaderData.ts
@@ -3,7 +3,8 @@ import { useRouteLoaderData } from "@remix-run/react";
 import type { Loader as RootLoader } from "~/root";
 
 export function useRootLoaderData() {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const data = useRouteLoaderData<RootLoader>(`root`);
 
-  return data;
+  return data as unknown;
 }
