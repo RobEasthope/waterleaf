6a1aa247bf895ebd081883766c2046da4c20067c
Rob Easthope <hello@robeasthope.com>
2024-07-12 13:41:01 +0100

    Add stylesCheck util

--- app/utils/tailwind.ts
+++ app/utils/tailwind.ts
@@ -1,6 +1,14 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Merge css classes together.
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+// Check if there are any styles to apply to the component. If there are no styles, return undefined.
+export function stylesCheck(styles: string | undefined) {
+  if (!styles) return undefined;
+
+  return styles;
 }
